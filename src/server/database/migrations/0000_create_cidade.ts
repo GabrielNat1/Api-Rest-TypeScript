9b312aad87b17d5ba7b18d6d038af4c4479b29d4
GabrielNat1 <dcbiel846@gmail.com>
2024-12-05 15:51:09 -0300

    feat: migrations

---
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -0,0 +1,20 @@
+import { Knex } from "knex";
+import { ETableNames } from "../Etablenames";
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable(ETableNames.cidade, (table) => {
+        table.bigIncrements('id').primary().index();
+        table.string('nome', 150).index().notNullable();
+        table.comment('Tabela para armazenar cidades');
+    })
+    .then(() => {
+        console.log(`# created table ${ETableNames.cidade}`);
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable(ETableNames.cidade) 
+    .then(() => {
+        console.log(`# dropped table ${ETableNames.cidade}`);
+    });
+}
