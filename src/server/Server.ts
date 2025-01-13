7a237a304dc5de4935131640e25f38252ba64560
Gabriel_devs <dcbiel846@gmail.com>
2025-01-12 23:50:09 -0300

    feat: add CORS support and update environment configuration

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 import './shared/services/translations';
@@ -7,6 +8,10 @@ import { router } from './routes';
 
 const server = express();
 
+
+server.use(cors({
+    origin: process.env.ENABLE_CORS?.split(';') || [],
+}));
 
 server.use(express.json());
 
